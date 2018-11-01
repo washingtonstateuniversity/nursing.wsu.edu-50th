0ce9b524691f0dc39adb13156ac4d9e5c4eb7152
danialbleile <danial.bleile@wsu.edu>
2018-11-01 15:16:37 -0700

    Fix header logo and link

---
+++ spine/header.php
@@ -0,0 +1,13 @@
+<header class="spine-header">
+	<a href="https://nursing.wsu.edu" id="wsu-signature">WSU COLLEGE OF NURSING</a>
+</header>
+
+<section id="wsu-actions" class="spine-actions clearfix">
+
+	<ul id="wsu-actions-tabs" class="spine-actions-tabs spine-tabs clearfix">
+		<li id="wsu-search-tab" class="spine-search-tab closed"><button>Search</button></li>
+		<li id="wsu-contact-tab" class="spine-contact-tab closed"><button>Contact</button></li>
+		<li id="wsu-share-tab" class="spine-share-tab closed"><button>Share</button></li>
+	</ul>
+
+</section><!--/#wsu-actions-->
